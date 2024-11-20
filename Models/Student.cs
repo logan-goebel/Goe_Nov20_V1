03a6863a65052e590e57716b829871f6c24e3f9f
13204 <13204@LOGANS-LAPTOP>
2024-11-20 13:44:28 -0600

    new Class Student

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Goe_Nov20.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? StudentLName { get; set; }
+        public string? StudentFName { get; set; }
+
+        
+    }
+}
